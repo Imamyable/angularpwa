155dffdccf422cd4bb85d18882c9a428232f241a
Imamyable <amynylander@hotmail.com>
2023-11-23 20:30:48 +0100

    d

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-E7WCX4P6.mjs";import"./chunk-UW7D6A3D.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-US23EJYA.mjs";import"./chunk-UW7D6A3D.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/camera/camera.component.ts
+++ src/app/camera/camera.component.ts
@@ -61,8 +61,9 @@ export class CameraComponent implements OnInit {
       .getUserMedia({
         video: {
           facingMode: 'environment',
-          width: { ideal: 1080 },
-          height: { ideal: 1920 },
+          width: { ideal: 1920 },
+          height: { ideal: 1080 },
+          aspectRatio: 9/16
         },
       })
       .then((stream) => {
