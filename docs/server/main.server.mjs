dbc8070ccd1fd5a000a821f510065b5518070a62
Imamyable <amynylander@hotmail.com>
2023-11-23 09:03:38 +0100

    new deploy

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-S5ACO546.mjs";import"./chunk-YKX4NCMO.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-GM7DS5Q3.mjs";import"./chunk-NAOT5DXB.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
